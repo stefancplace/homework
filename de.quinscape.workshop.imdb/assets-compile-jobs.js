ae46490cc54d165e7fedd13b4f4e8deb01865eb1
Thomas Buechner <thomas.buechner@collaboration-factory.de>
2018-09-30 17:37:21 +0200

    removed plugin

--- de.quinscape.workshop.imdb/assets-compile-jobs.js
+++
@@ -1,1 +0,0 @@
-new TscJob(repoName, pluginName, 'TypeScript for imdb');
--- de.quinscape.workshop.imdb/assets/ts/_app.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path='../../../../main/cf.cplace.platform/assets/ts/globals.d.ts' />
-/// <reference path='../../../../main/cf.cplace.platform/assets/typings/index.d.ts' />
-/// <reference path='../../../../main/cf.cplace.platform/assets/generated_js/tsc.d.ts' />
