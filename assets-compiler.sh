d01e536d152e812cac692f14734508354f8f5b6a
Bran Kohlschreiber <bran.kohlschreiber@consol.de>
2019-02-22 13:51:55 +0100

    updated assets compiler

--- assets-compiler.sh
+++ assets-compiler.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-
-while [[ true ]]; do
-    node ../main/assets-compiler.js -doWatchCompiler -repo cplace-quinscape-workshop -chdir ../main %1 %2 %3 %4
-    result=${?}
-    if [[ $result == 1 ]]; then
-        echo "Got result: ${result} - stopping."
-        break;
-    fi
-    echo "Compiler finished - restarting!"
-done
+cplace-asc -w -c "$@"
